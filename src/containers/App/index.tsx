9555e2ce594cc8886b1c4f70f87c1c4b46535ef7
Shir Yehuda <shiry@wix.com>
2021-05-24 18:36:14 +0300

    pull tryout

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
                            ></TodoList>}
                     </div>
                     //fghfghv
-
+//
                 }
             </div>
         </div>
