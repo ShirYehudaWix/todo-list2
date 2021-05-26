2580aadf2cb5179197a5a186b1f7de0b1df24b8e
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-05-26 19:18:21 +0300

    Connect redux (#6)
    * first phase is over

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -7,35 +7,35 @@ import {todoTask} from "../../utils/interfaces";
 import {AddButton} from "../AddButton";
 
 const App: React.FC = () => {
-    const lst:todoTask[] = [
-        // {id: 1, task: "go to the market",labels: "",finished:true},
-        // {id: 2, task: "feed the dog",labels:"Home", finished: false},
-        // {id:3, task: "feed the dog",labels:"Garden", finished: false}
-
-    ]
+    const lst:todoTask[] = [ ]
 
     const [isAddTaskOpen,setIsAddTaskOpen]=React.useState(false)
     const [todolst,setTodolst]=React.useState([] as todoTask[]);
-    const [isEmpty, setIsEmpty] = React.useState(true)
-    const setIsEmptyDecorator=()=>setIsEmpty(false);
 
 
     return (
         <div className={style.root}>
-            <Header></Header>
+            <Header />
             <AddButton isAddTaskOpen={isAddTaskOpen} setIsAddTaskOpen={setIsAddTaskOpen}></AddButton>
             <div className={style.middlePage}>
-                {isAddTaskOpen&&<AddTask setIsAddTaskOpen={setIsAddTaskOpen} setTodolst={setTodolst} todolst={todolst}
-                                                       setIsEmpty={setIsEmptyDecorator}></AddTask>}
+                { isAddTaskOpen && 
+                    <AddTask 
+                        setIsAddTaskOpen={setIsAddTaskOpen} 
+                        setTodolst={setTodolst} 
+                        todolst={todolst}
+                    />
+                }
                 {
-                    !isAddTaskOpen&&<div className={style.flexContained}>
-                        {isEmpty ? <img className={style.img}
-                                        src="https://organisemyhouse.com/wp-content/uploads/2013/04/143.-EMPTY-TO-DO-LIST-2.jpg"></img>
+                    !isAddTaskOpen &&
+                    <div className={style.flexContained}>
+                        {
+                           todolst.length==0 ?
+                                <img className={style.img} src="https://organisemyhouse.com/wp-content/uploads/2013/04/143.-EMPTY-TO-DO-LIST-2.jpg"></img>
                             :
                             <TodoList
                                 todolst={todolst}
                                 setTodolst={setTodolst}
-                           ></TodoList>}
+                           />}
                     </div>
                 }
             </div>
