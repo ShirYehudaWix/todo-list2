945ce7717d05e2cde1a57274f1d4e0273ffb278f
ShirYehudaWix <shiry@wix.com>
2025-03-25 11:58:48 +0200

    Revert "changed color"
    This reverts commit 2e72446bc06184b593ffd0eb25b84a4882a9b83f.

--- src/components/addTask/ConnectedAddTask.tsx
+++ src/components/addTask/ConnectedAddTask.tsx
@@ -6,7 +6,7 @@ import {todoTask} from "../toDoList/task/types";
 import {DispatchForAddTask} from "./types";
 
 const mapStateToProps = (state:StoreState) => {
-    return {optionList: state.optionList,aaa:'sfsdfsdfwef'}
+    return {optionList: state.optionList}
 }
 
 const mapDispatchToProps = (dispatch: DispatchForAddTask) => {
